2681e65169b2d61e5d59535868dacd1f6e0aba8f
Muhammad Abdurrahman <rachman.sd@gmail.com>
2021-02-04 14:59:49 +0800

    remove yarn log

--- src/inview-action.ts
+++ src/inview-action.ts
@@ -4,14 +4,19 @@ import {extraArgument} from "./ArgumentHandler"
 import inView from "in-view"
 
 const nanoid = customAlphabet("nal0xIpcM6Cb4E58QuI9yjtYp1eUPe2XmBkrWN3hVBoS7DGSiTLyFsZ", 10)
+
+const inviewAction = (el: HTMLElement) => {
+
+}
+
 const VueInview = (el: HTMLElement, binding: any): void => {
     // generate class Name
     const classID = "inview-" + nanoid()
 
     // add ClassName to ClassList
     ClassList.init(el).add(classID)
-
-    const defineHandler = !IsDefine(binding.arg) ? "on" : binding.arg === "once" ? "once" : extraArgument(binding.arg) !== "none" ? "on" : "none"
+    const extrArg = extraArgument(binding.arg)
+    const defineHandler = !IsDefine(binding.arg) ? "on" : binding.arg === "once" ? "once" : extrArg !== "none" ? "on" : "none"
 
     if (defineHandler === "none") {
         console.error(`argument ${binding.arg} undefined`)
@@ -19,7 +24,9 @@ const VueInview = (el: HTMLElement, binding: any): void => {
     }
 
     const setupInView = inView("." + classID)
-    if (defineHandler === "on") setupInView.on("enter")
+    const eventHandler = extrArg === 'leave' ? "exit" : "enter"
+    if (defineHandler === "on") setupInView.on(eventHandler, inviewAction)
+    if (defineHandler === "once") setupInView.once(eventHandler, inviewAction)
 
 }
 
